f6951ff721266af4290091e890530fe3db50c858
Vedanta Jha <jhavedanta@hotmail.com>
2019-10-29 11:13:24 +0530

    Update test_globalmatting.cpp

--- modules/ximgproc/test/test_globalmatting.cpp
+++ modules/ximgproc/test/test_globalmatting.cpp
@@ -6,79 +6,79 @@ using namespace cv;
 
 namespace opencv_test
 {
-namespace
-{
-const std::string INPUT_DIR       = "cv/ximgproc";
-const std::string IMAGE_FILENAME  = "input/doll.png";
-const std::string TRIMAP_FILENAME = "trimap/doll.png";
-
-
-class CV_GlobalMattingTest
-{
-  public:
-     CV_GlobalMattingTest();
-
-  protected:
-     Ptr<GlobalMatting> gm;
-     virtual void run(int);
-     void runModel();
-
-};
-
-void CV_GlobalMattingTest::runModel()
-{
-  std::string folder       = std::string(cvtest::TS::ptr()->get_data_path());
-  std::string img_path     = folder + INPUT_DIR + "/" + IMAGE_FILENAME;
-  std::string trimap_path  = folder + INPUT_DIR + "/" + TRIMAP_FILENAME;
-
-  Mat img     = cv::imread(img_path,cv::IMREAD_COLOR);
-  Mat trimap  = cv::imread(trimap_path,cv::IMREAD_GRAYSCALE);
-  if(img.empty() || trimap.empty())
+  namespace
   {
-    ts->printf(cvtest::TS::LOG,"Test images not found!\n");
-    ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_TEST_DATA);
-    return;
-  }
-  if(img.cols!=trimap.cols || img.rows!=trimap.rows)
+  const std::string INPUT_DIR       = "cv/ximgproc";
+  const std::string IMAGE_FILENAME  = "input/doll.png";
+  const std::string TRIMAP_FILENAME = "trimap/doll.png";
+
+
+  class CV_GlobalMattingTest : public cvtest::BaseTest
   {
-    ts->printf(cvtest::TS::LOG,"Dimensions of trimap and the image are not the same");
-    ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_TEST_DATA);
-    return;
-  }
-  Mat foreground,alpha;
-  int niter = 9;
-  this->gm->getMat(img,trimap,foreground,alpha,niter);
-  if(alpha.empty())
+    public:
+       CV_GlobalMattingTest();
+
+    protected:
+       Ptr<GlobalMatting> gm;
+       virtual void run(int);
+       void runModel();
+
+  };
+
+  void CV_GlobalMattingTest::runModel()
   {
-    ts->printf(cvtest::TS::LOG,"Could not find the alpha matte for the image\n");
-    ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
-    return;
+    std::string folder       = std::string(cvtest::TS::ptr()->get_data_path());
+    std::string img_path     = folder + INPUT_DIR + "/" + IMAGE_FILENAME;
+    std::string trimap_path  = folder + INPUT_DIR + "/" + TRIMAP_FILENAME;
+
+    Mat img     = cv::imread(img_path,cv::IMREAD_COLOR);
+    Mat trimap  = cv::imread(trimap_path,cv::IMREAD_GRAYSCALE);
+    if(img.empty() || trimap.empty())
+    {
+      ts->printf(cvtest::TS::LOG,"Test images not found!\n");
+      ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_TEST_DATA);
+      return;
+    }
+    if(img.cols!=trimap.cols || img.rows!=trimap.rows)
+    {
+      ts->printf(cvtest::TS::LOG,"Dimensions of trimap and the image are not the same");
+      ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_TEST_DATA);
+      return;
+    }
+    Mat foreground,alpha;
+    int niter = 9;
+    this->gm->getMat(img,trimap,foreground,alpha,niter);
+    if(alpha.empty())
+    {
+      ts->printf(cvtest::TS::LOG,"Could not find the alpha matte for the image\n");
+      ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
+      return;
+    }
+
+    if(alpha.cols!=img.cols || alpha.rows!=img.rows)
+    {
+      ts->printf(cvtest::TS::LOG,"The dimensions of the output are not correct");
+      ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
+      return;
+    }
   }
 
-  if(alpha.cols!=img.cols || alpha.rows!=img.rows)
+  CV_GlobalMattingTest::CV_GlobalMattingTest()
   {
-    ts->printf(cvtest::TS::LOG,"The dimensions of the output are not correct");
-    ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
-    return;
+    gm = makePtr<GlobalMatting>();
   }
-}
-
-CV_GlobalMattingTest::CV_GlobalMattingTest()
-{
-  gm = makePtr<GlobalMatting>();
-}
-void CV_GlobalMattingTest::run(int)
-{
-  runModel();
-}
+  void CV_GlobalMattingTest::run(int)
+  {
+    runModel();
+  }
 
 
 
-TEST(CV_GlobalMattingTest,accuracy)
-{
-  CV_GlobalMattingTest test;
-  test.safe_run();
-}
+  TEST(CV_GlobalMattingTest,accuracy)
+  {
+    CV_GlobalMattingTest test;
+    test.safe_run();
+  }
 
 }
 }
